355e0c70990d946626274ca4fa5f61f956b4a3ca
vaarigupta <vaarigupta24@gmail.com>
2018-10-02 22:57:08 +0530

    express used

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+
+var app = express();
+
+
+app.get('/',(request , response)=>{
+
+response.send('<h1>hey babes</h1>');
+})
+
+app.get('/json',(request , response)=>{
+	response.send({
+		name : 'vaari',
+		age : 21,
+		college : 'bvcoe',
+		branch : 'IT',
+		hobbies : [
+		'dancing',
+		'reading',
+		'listening music']
+	})
+
+})
+
+app.get('/webpage',()=>{
+	response.send('./Facebook_clone/public/fb.html');
+})
+
+app.get('/image',(request,response)=>{
+	response.send('./space5.jpg')
+})
+
+
+app.get('/bad',(request,response)=>{
+	response.send(
+	{
+		errorMessage : '404 Error'
+	})
+
+})
+
+
+app.listen(3000);
