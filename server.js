d3ad44b18d3896f38c5ba3d141f9d26becd37e7f
vaarigupta <vaarigupta24@gmail.com>
2018-10-13 10:19:59 +0530

    hbs next() used

--- server.js
+++ server.js
@@ -1,24 +1,38 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 var app = express();
 
 app.set('view engine','hbs');
-//app.use(express.static(__dirname + '/public'));
+
 // app.get('/',(request , response)=>{
 
 // response.send('<h1>hey babes</h1>');
 // })
+app.use((req,res,next)=>{
+	var now = new Date().toString();
+	var log = `${now} : ${req.method} : ${req.url} \n`;
+	console.log(log);
+	fs.appendFileSync('server.log',log);
 
 
+	next();
+
+})
+app.use((req,res,next)=>{
+	res.render('maintenance.hbs');
+})
+app.use(express.static(__dirname + '/public'));
 hbs.registerPartials(__dirname + '/views/partials');
 hbs.registerHelper('getCurrentYear',()=>{
-	return new Date().getFullYear()
+	return new Date().getFullYear();
 
 })
 
 hbs.registerHelper('upperCase',(text)=>{
    return text.toUpperCase();
 })
+
 app.get('/',(req,res)=>{
 	res.render('home.hbs',{
 		Heading : 'Home Page',
